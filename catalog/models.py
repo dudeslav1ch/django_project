676bb4dac9662d8b4ed0b0a12ec6e435acd8cd38
dudeslav1ch <dudeslav1ch@gmail.com>
2024-04-11 04:11:36 +0300

    hw-3

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=100, verbose_name='Наименование')
+    category_description = models.CharField(verbose_name='Описание', **NULLABLE)
+
+    def __str__(self):
+        return f'{self.category_name}'
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Категория')
+    products_name = models.CharField(max_length=100, verbose_name='Наименование')
+    products_description = models.TextField(verbose_name='Описание', **NULLABLE)
+    image = models.ImageField(upload_to='products/', verbose_name='Изображение', **NULLABLE)
+    price = models.IntegerField(verbose_name='Цена за покупку')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания', **NULLABLE)
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Дата последнего изменения', **NULLABLE)
+
+    def __str__(self):
+        return f'{self.category} {self.products_name}'
+
+    class Meta:
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
+        ordering = ('products_name',)
---
+++ config/management/commands/fill.py
@@ -0,0 +1,59 @@
+import json
+import pathlib
+
+from django.core.management import BaseCommand
+from django.db import connection
+
+from catalog.models import Category, Product
+
+ROOT = pathlib.Path(__file__).parent.parent.parent.parent
+DATA_CATEGORY = pathlib.Path(ROOT, 'data', 'category.json')
+DATA_PRODUCT = pathlib.Path(ROOT, 'data', 'product.json')
+
+
+class Command(BaseCommand):
+
+    @staticmethod
+    def json_read_categories() -> list:
+        # Здесь мы получаем данные из фикстур с категориями
+        with open(DATA_CATEGORY) as file:
+            file_info = json.load(file)
+        return [info for info in file_info]
+
+    @staticmethod
+    def json_read_products() -> list:
+        # Здесь мы получаем данные из фикстур с продуктами
+        with open(DATA_PRODUCT) as file:
+            file_info = json.load(file)
+        return [info for info in file_info]
+
+    def handle(self, *args, **options):
+        # Очистка базы данных перед заполнением
+        Category.objects.all().delete()
+        Product.objects.all().delete()
+
+        category_for_create = []
+        product_for_create = []
+
+        with connection.cursor() as cursor:
+            cursor.execute(
+                "TRUNCATE TABLE catalog_category, catalog_product RESTART IDENTITY CASCADE;")
+
+        # Заполнение категорий
+        for category in Command.json_read_categories():
+            category_fields = category.get('fields')
+            category_for_create.append(
+                Category(category_title=category_fields.get('category_name'),
+                         category_description=category_fields.get('category_description'))
+            )
+
+        Category.objects.bulk_create(category_for_create)
+
+        # Заполнение продуктов
+        for product in Command.json_read_products():
+            product_fields = product.get('fields')
+            product_for_create.append(
+                Product(product_title=product_fields.get('products_name'),
+                        category=Category.objects.get(pk=product_fields.get('category')))
+            )
+        Product.objects.bulk_create(product_for_create)
